3b0374f8512ee9523a5ec6a39dfac574c2cf6c1e
Praveen-1992 <praveenmailoor@gmail.com>
2024-12-02 22:55:14 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,9 @@
 
 # Run the ProxySniffer Java program in the background with specified memory settings
 java -Xmx768m ProxySniffer -JobControlTler -ExecAgent -encryptJobsOriginBackup -tz CS &
-# Forward all arguments to the main command
+
+# Debugging: Show the arguments being passed to exec
+echo "Arguments passed: $@"
+
+# Forward all arguments to the main command if valid (add validation if needed)
 exec "$@"
