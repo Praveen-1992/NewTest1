71e710150fc6787cc86bfc3b8ee30cc9a4b7caa2
Praveen-1992 <praveenmailoor@gmail.com>
2024-12-02 22:15:39 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-# Set the CLASSPATH environment variable
-export CLASSPATH=".:/home/pravi/ZebraTester/Container/xercesImpl-2.11.0.jar:/home/pravi/ZebraTester/Container/lib/*"
-
-# Run the Java program with the specified memory option and parameters
-java -Xmx768m Platform_Prod_US_v2 -u 1 -d 90 -t 60 -sdelay 200 -maxloops 1 -sampling 15 -percpage 100 -percurl 20 -maxerrmem 20 -nolog
+echo "Running ZebraTester with arguments: $@"
+java -Xmx1024m -classpath "$CLASSPATH" Platform_Prod_US_v2 "$@"
